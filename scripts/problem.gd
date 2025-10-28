be40f48d87968e1bd76dd5b998ea5c08788124ef
valet noir <valetnoir.gaming@gmail.com>
2025-10-28 14:23:26 +0100

    # problems

--- scripts/problem.gd
+++ scripts/problem.gd
@@ -2,9 +2,13 @@ extends Node
 
 @onready var timer: Timer = $Timer
 
+@export var plant: Node
+
 @export var unlock_time = 10
 @export var reload_time = 10
 @export_range (0, 20) var reload_time_variation: float = 10
+
+var isActive = false
 
 signal started()
 signal stopped()
@@ -12,6 +16,12 @@ signal stopped()
 func _ready() -> void:
 	timer.wait_time = unlock_time
 	timer.start()
+
+func _process(delta: float) -> void:
+	if not isActive:
+		return
+	
+	plant.life -= 1 * delta
 
 func start() -> void:
 	started.emit()
