341bfb1f86ebb3c6868e019492b25a0f9804365c
valet noir <valetnoir.gaming@gmail.com>
2025-10-29 00:15:30 +0100

    maniement mainscene

--- scripts/plantActivityTrigger.gd
+++ scripts/plantActivityTrigger.gd
@@ -6,5 +6,5 @@ extends Node
 func _unhandled_input(event: InputEvent) -> void:
 	if linkedProblem.is_active and !Data.is_occupied && event is InputEventMouseButton && event.is_pressed():
 		var solution_instance = linkedSolution.instantiate()
-		linkedProblem.add_child(solution_instance)
+		get_tree().get_root().add_child(solution_instance)
 		solution_instance.problem = linkedProblem
