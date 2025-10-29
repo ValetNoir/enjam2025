bb07a17268b6842d975d32450ea719eec9a50462
valet noir <valetnoir.gaming@gmail.com>
2025-10-29 08:25:53 +0100

    visu arrosoir et priere

--- scripts/hold.gd
+++ scripts/hold.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
+@export var cursor_sprite: Resource
+
 @onready var timer: Timer = $Timer
+@onready var progress_bar: ProgressBar = $ProgressBar
 
 var mouse_inside_area = false
 
@@ -9,18 +12,26 @@ signal finished()
 func _ready() -> void:
 	timer.start()
 	timer.paused = true
+	Input.set_custom_mouse_cursor(cursor_sprite)
+	progress_bar.max_value = timer.wait_time
+	progress_bar.hide()
 
 func _process(_delta: float) -> void:
+	progress_bar.global_position = get_global_mouse_position() - Vector2(0, 65)
+	progress_bar.value = timer.wait_time - timer.time_left
 	if mouse_inside_area and Input.is_action_pressed("click"):
 		timer.paused = false
 	else:
 		timer.paused = true
 
 func _on_area_2d_mouse_entered() -> void:
+	progress_bar.show()
 	mouse_inside_area = true
 
 func _on_area_2d_mouse_exited() -> void:
+	progress_bar.hide()
 	mouse_inside_area = false
 
 func _on_timer_timeout() -> void:
+	Input.set_custom_mouse_cursor(null)
 	finished.emit()
