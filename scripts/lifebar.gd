0c4fe47ecfd7b68c58b5897895af47ef036faa49
valet noir <valetnoir.gaming@gmail.com>
2025-10-29 01:31:59 +0100

    base gameplay

---
+++ scripts/lifebar.gd
@@ -0,0 +1,4 @@
+extends ProgressBar
+
+func _process(_delta: float) -> void:
+	value = Data.plant_health
--- scripts/solution.gd
+++ scripts/solution.gd
@@ -3,9 +3,9 @@ extends Node
 var problem: Node
 
 func _ready() -> void:
-	Data.is_occupied = false
+	Data.is_occupied = true
 
 func finish() -> void:
-	Data.is_occupied = true
+	Data.is_occupied = false
 	problem.stop()
 	queue_free()
